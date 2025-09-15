bf790913117e1cb7bea27e25a7c42c843a8a13f7
Maximo Blazquez <maxi05blazquez@gmail.com>
2025-09-15 08:58:34 -0300

    add grados a radianes

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -1,8 +1,8 @@
 /**
- * Descripción
- * @method Nombre de la función
- * @param Parámetro A
- * @param Parámetro B
+ * convierte las unidades ingresadas por el usuario
+ * @method convertirUnidades
+ * @param {string} unidad - unidad de valor ingresada: metro pie pulgada
+ * @param {number} valor - numero de la unidad antes dicho 
  * @return Valor que retorna
  */
 
@@ -61,4 +61,27 @@ function conversor(unidad,valor){
     document.getElementById("pie").value = pie;
     document.getElementById("pulgada").value = pulgada;
     document.getElementById("yarda").value = yarda;
+}
+
+
+/**
+ * convierte grados a radienes y vicebersa
+ * @method convertirUnidades
+ * @param {string} unidad - grados o radianes
+ * @param {number} valor - numero de la unidad antes dicho 
+ * @return Valor que retorna
+ */
+
+let convertirGR = (id,valor) => {
+    let cantGrados, cantRadianes;
+    if(id == "grados"){
+        cantGrados = valor;
+        cantRadianes = cantGrados*Math.PI/180;
+        document.getElementById("radianes").value = cantRadianes;
+    }else
+    {
+        cantRadianes = valor;
+        cantGrados = cantRadianes*180/Math.PI;
+        document.getElementById("grados").value = cantGrados;
+    }
 }
